b2f0df8ff059b8633fa201638e566ef158abc9b6
DEEPAKGUPTA1141421 <deepak2198.be21@chitkara.edu.in>
2024-01-18 14:50:10 +0530

    here

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const express=require("express");
 const app=express();
+const cookie=require("cookie-parser");
+var jwt = require('jsonwebtoken');
 const port=8000;
+app.use(cookie());
 const server=app.listen(port,()=>{
     console.log("server is running on port "+port);
-})
+});
 module.exports=app;
