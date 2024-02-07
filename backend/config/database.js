f755cb0cd4eeb6e613e12dbbe7e86da4ab4a3673
DEEPAKGUPTA1141421 <103554042+DEEPAKGUPTA1141421@users.noreply.github.com>
2024-02-07 15:36:03 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,5 +1,6 @@
 const mongoose=require("mongoose");
 const connectDb=()=>{
+    console.log("hello");
     const promiseReturByMongoose=mongoose.connect(process.env.MONGODB_URL);
     promiseReturByMongoose.then(()=>{
         console.log("connection Successful");
