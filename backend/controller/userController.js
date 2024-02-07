846d0027cfbf131bad849cae610b221c7f54c8fa
DEEPAKGUPTA1141421 <103554042+DEEPAKGUPTA1141421@users.noreply.github.com>
2024-02-07 15:20:53 +0530

    Update userController.js

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,29 +1,7 @@
-<<<<<<< HEAD
-=======
-
-
->>>>>>> origin/main
 const customError = require("../middleware/customError");
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 const sendEmail = require("../utils/sendMail");
-//const sendToken = require("../utils/sendToken");
-<<<<<<< HEAD
-
-const sendToken = (user, statusCode, message, res) => {
-  const token = user.getToken();
-  console.log(token);
-  res.cookie("token", token, {
-    expiresIn: new Date(Date.now() + 10 * 24 * 60 * 60 * 1000),
-    httpOnly: true,
-  });
-
-  res.status(statusCode).json({
-    user: user,
-    success: true,
-    message: message,
-  });
-=======
 const sendToken=async(user,statusCode,message,res)=>{
   const token=user.getToken();
   console.log("hello");
@@ -38,49 +16,56 @@ const sendToken=async(user,statusCode,message,res)=>{
       message:message,
       success:true
   })
->>>>>>> origin/main
 };
 
 module.exports.signup = async (req, res, next) => {
-  console.log("indside");
+  console.log("inside");
   const { name, email, password } = req.body;
-  console.log("inside controller");
-  console.log(name, email, password);
-  const user = await User.findOne({ email: email });
-  if (user) {
-    next(new customError("User Already Exists", 400));
-    return; // Added return to prevent the code below from executing
-  }
-  const newUserToBeCreated = {
-<<<<<<< HEAD
-    fullname: name,
-    email: email,
-    password: password,
-    image: "req.file.filename", // Corrected the image property
-  };
-  const activationToken = createActivationToken(newUserToBeCreated);
-=======
-   fullname: name,
-   email: email,
-   password: password,
-   image: "req.file.filename", // Corrected the image property
- };
-  const activationToken =  createActivationToken(newUserToBeCreated);
->>>>>>> origin/main
-
-  const activationUrl = `http://localhost:3000/activation/${activationToken}`;
+  
   try {
+    const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
+      folder: "avatars",
+      width: 150,
+      crop: "scale"
+    });
+    
+    console.log("inside controller");
+    console.log(name, email, password);
+  
+    const user = await User.findOne({ email: email });
+  
+    if (user) {
+      throw new customError("User Already Exists", 400); // Throw error instead of using next()
+    }
+  
+    const newUserToBeCreated = {
+      fullname: name,
+      email: email,
+      password: password,
+      image: myCloud.url // Accessing the URL property of myCloud directly
+    };
+  
+    const activationToken = createActivationToken(newUserToBeCreated);
+  
+    const activationUrl = `http://localhost:3000/activation/${activationToken}`;
+  
+    console.log(newUserToBeCreated.email);
+  
     await sendEmail({
       email: newUserToBeCreated.email,
       subject: "Activate Your Account",
       message: `Hello ${newUserToBeCreated.fullname} Please Click To Activate On The Link For Your Account: ${activationUrl}`,
     });
+  
+    res.status(200).json({
+      success: true,
+      user:"user needs to br crested",
+      message: "email sent"
+    });
   } catch (err) {
-    next(new customError(err.message, 500));
-    return; // Added return to prevent the code below from executing
+    next(new customError(err.message, err.statusCode || 500)); // Pass the status code if available
   }
 };
-<<<<<<< HEAD
 module.exports.createActualUser = async (req, res, next) => {
   console.log("called Actual");
   const { activationToken } = req.body;
@@ -101,35 +86,6 @@ module.exports.createActualUser = async (req, res, next) => {
     next(new customError(err.message, 500));
   }
 };
-const createActivationToken = (user) => {
-  return jwt.sign({ id: user }, process.env.jwtActivationSecret, {
-    expiresIn: "5m",
-  });
-};
-
-module.exports.login = async (req, res, next) => {
-  let data = req.body;
-  const { email, password } = data;
-  const user = await User.findOne({ email: email }).select("+password");
-  console.log("abgj", user);
-=======
-module.exports.createActualUser=async(req,res,next)=>{
-   console.log("calledd Actual");
-   const {activationToken}=req.body;
-   console.log("activationToken", activationToken);
-   const newuser=await jwt.verify(activationToken, process.env.jwtActivationSecret);
-   console.log(newuser);
-   const {id}=newuser;
-   if(!newuser){
-      next(new customError("ToKen Expired",400));
-   }
-   try {
-      const u = await User.create({ ...id });
-      sendToken(u,201,"User Created Succesfully",res);
-    } catch (err) {
-      next(new customError(err.message, 500));
-    }
-}
 const createActivationToken = (user) =>{
   return jwt.sign({ id:user}, process.env.jwtActivationSecret, {
     expiresIn: "5m",
@@ -138,9 +94,8 @@ const createActivationToken = (user) =>{
 module.exports.login = async (req, res, next) => {
   let data = req.body;
   const { email, password } = data;
-  const user = await User.findOne({ email:email }).select("password");
+  const user = await User.findOne({ email:email }).select("+password");
   console.log(user);
->>>>>>> origin/main
   if (!user) {
     res.status(404).json({
       success: false,
@@ -148,38 +103,23 @@ module.exports.login = async (req, res, next) => {
     });
   } else {
     try {
-<<<<<<< HEAD
-      const passMatch = await user.checkPassword(password);
-=======
       console.log("1");
       const passMatch = await user.checkPassword(password);
       console.log("2");
->>>>>>> origin/main
       if (!passMatch) {
         res.status(401).json({
           success: false,
           message: "Unauthorized user",
         });
       } else {
-<<<<<<< HEAD
-        sendToken(user, 201, "user logged in successfully", res);
-      }
-    } catch (err) {
-      console.log("err");
-=======
-        res.status(200).json({
-          success: true,
-          message: "user logged in successfully",
-        });
+        sendToken(user,201,"Login Succesful",res);
       }
     } catch (err) {
       console.log("sere");
->>>>>>> origin/main
       next(new customError(err.message, 400));
     }
   }
 };
-<<<<<<< HEAD
 
 module.exports.getUser = async (req, res, next) => {
   let id = req.params.id;
@@ -246,5 +186,3 @@ module.exports.deleteUser = async (req, res, next) => {
     next(new customError(err));
   }
 };
-=======
->>>>>>> origin/main
