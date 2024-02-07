b74a18fc1b95543f23dc228f1df4f215d8ff0587
surbhi-kuhar <104093804+surbhi-kuhar@users.noreply.github.com>
2024-02-07 15:17:18 +0530

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require("express");
 const {
   signup,
@@ -8,31 +7,7 @@ const {
   updateUser,
   deleteUser
 } = require("../controller/userController");
-const router = express.Router();
-=======
-const express=require("express");
-const { signup,createActualUser, login } = require("../controller/userController");
 const router=express.Router();
->>>>>>> origin/main
-//const {upload}=require("../multer.js");
-const multer = require("multer");
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-<<<<<<< HEAD
-    cb(null, "../uploads");
-=======
-    cb(null, '../uploads');
->>>>>>> origin/main
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now();
-    cb(null, uniqueSuffix + file.originalname);
-<<<<<<< HEAD
-  },
-});
-
-const upload = multer({ storage: storage });
 
 router.post("/signup", upload.single("file"), signup);
 router.post("/activation", upload.single("file"), createActualUser);
@@ -41,14 +16,6 @@ router.get("/get/:id", getUser);
 router.put("/update/:id", updateUser);
 router.delete("/delete/:id", deleteUser);
 module.exports = router;
-=======
-  }
-});
 
-const upload = multer({ storage: storage });
-console.log("in Routs");
-router.post('/signup',upload.single("file"),signup);
-router.post("/activation",upload.single("file"),createActualUser);
-router.post("/login",upload.single("file"),login);
-module.exports=  router;
->>>>>>> origin/main
+
+
